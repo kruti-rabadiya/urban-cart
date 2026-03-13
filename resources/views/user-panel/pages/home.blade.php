ebc38448c615a1f85f3fd5f418f1ed06fe289118
Kruti <kruti.rabadiya.git@gmail.com>
2026-03-13 10:05:04 +0530

    update home page with promo banner and new arrivals section, and enhance styles

--- resources/views/user-panel/pages/home.blade.php
+++ resources/views/user-panel/pages/home.blade.php
@@ -14,16 +14,18 @@
 
 @section('content')
 
-    <section class="home-hero">
+    {{-- Home Hero Section --}}
+    <section class="home-hero" style="background-image:url('{{ asset('web-images/hero8.png') }}')">
 
         <div class="home-hero-container">
 
             <h1 class="home-hero-title">
-                Effortless Style
+                Elevate Your Wardrobe
             </h1>
 
             <p class="home-hero-subtitle">
-                Discover modern fashion designed for comfort, confidence, and everyday wear.
+                Discover modern fashion designed for confidence
+                and effortless everyday elegance.
             </p>
 
             <div class="home-hero-btns">
@@ -113,7 +115,62 @@
             </div> --}}
         </div>
     </section>
-    
+
+    {{-- promo banner --}}
+    <section class="home-promo-banner" style="background-image: url('{{ asset('web-images/promo-banner.jpg') }}')">
+
+        <div class="home-promo-banner-content">
+
+            <h2 class="home-promo-banner-title">
+                Modern Essentials
+            </h2>
+
+            <p class="home-promo-banner-subtitle">
+                Discover timeless fashion designed for comfort, confidence and everyday elegance.
+            </p>
+
+            <a href="#ShopNow" class="promo-banner-btn">
+                Shop Collection
+            </a>
+
+        </div>
+
+    </section>
+
+    <section class="home-section home-new-arrivals">
+        <div class="home-section-content">
+            <div class="home-heading">
+                <div class="home-head">
+                    <div class="home-head-line"></div>
+                    <h2>
+                        New Arrivals
+                    </h2>
+                    <div class="home-head-line"></div>
+                </div>
+                <div class="home-head-para">
+                    <p>Check out our latest additions to the collection.</p>
+                </div>
+            </div>
+            <div class="home-card-grid-section">
+                <div class="home-card-grid">
+
+                    @for ($i = 0; $i <= 7; $i++)
+                        <x-user-panel.product-card />
+                    @endfor
+                </div>
+
+            </div>
+            <div class="carousel-dots-section">
+                <div class="carousel-dots-scroll">
+                    <div class="crousel-dots">
+                        @for ($j = 0; $j <= 7; $j++)
+                            <span class="carousel-dot{{ $j === 0 ? ' active' : '' }}"></span>
+                        @endfor
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     <script>
         const crousleDots = document.querySelectorAll('.carousel-dot');
         crousleDots.forEach((dot, index) => {
